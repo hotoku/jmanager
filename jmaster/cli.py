fdf6995eb7391af97702c5c2722be7bbb0d57eb3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-05 13:46:21 +0900

    wip

--- jmaster/cli.py
+++ jmaster/cli.py
@@ -1,15 +1,6 @@
 """Console script for jmaster."""
-import sys
-import click
 from jmaster import jmaster
 
 
-@click.command()
-def main(args=None):
-    """Console script for jmaster."""
-    jmaster.run()
-    return 0
-
-
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    print(1)
--- jmaster/jmaster.py
+++ jmaster/jmaster.py
@@ -6,30 +6,53 @@ from time import sleep
 import re
 import json
 import sys
+import click
 
 _PID_FILE = "jupyter.pid"
 _LOG = "jupyter.log"
 
 
-def launch():
-    with open(_LOG, "w") as f:
-        po = sp.Popen(["jupyter", "lab"],
-                      stdout=f,
-                      stderr=sp.STDOUT,
-                      text=True)
-    sleep(5)
-    with open(_LOG) as f:
-        for l in f:
-            r = re.match(r"^ +http://localhost:([0-9]+)/\?token=([0-9a-z]+)$", l)
-            if r:
-                port, token = r.expand(r"\1"), r.expand(r"\2")
-    with open(_PID_FILE, "w") as f:
-        dat = dict(
-            pid=po.pid,
-            port=int(port),
-            token=token
-        )
-        json.dump(dat, f)
+class Launcher:
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        os.remove(_PID_FILE)
+
+    def launch(self):
+        with open(_LOG, "w") as f:
+            po = sp.Popen(["jupyter", "lab"],
+                          stdout=f,
+                          stderr=sp.STDOUT,
+                          text=True)
+        sleep(5)
+        with open(_LOG) as f:
+            for l in f:
+                r = re.match(r"^ +http://localhost:([0-9]+)/\?token=([0-9a-z]+)$", l)
+                if r:
+                    port, token = r.expand(r"\1"), r.expand(r"\2")
+        with open(_PID_FILE, "w") as f:
+            dat = dict(
+                pid=po.pid,
+                port=int(port),
+                token=token
+            )
+            json.dump(dat, f)
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def main(ctx):
+    if ctx.invoked_subocmmand is None:
+        run()
+
+
+@main.command()
+def kill():
+    if os.exists(_PID_FILE):
+        with open(_PID_FILE) as f:
+            pid = json.load(f)
+        sp.Popen(["kill", "-9", pid["pid"]])
 
 
 def open_browser():
@@ -38,10 +61,12 @@ def open_browser():
     sp.Popen(["open", f"http://localhost:{dat['port']}/?token={dat['token']}"])
 
 
+@main.command()
 def run():
     if not os.path.exists("jupyter.pid"):
         sys.stderr.write("launching new jupyter process\n")
-        launch()
+        launcher = Launcher()
+        launcher.launc()
     else:
         sys.stderr.write("jupyter process already run\n")
         open_browser()
