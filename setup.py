5444de4120bed362a7fc79496d2c58fb7c75d9d0
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-07 10:12:49 +0900

    requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'jupyter']
+requirements = ['Click>=7.0', 'jupyterlab']
 
 setup_requirements = []
 
