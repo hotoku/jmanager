f4f2b40753057a33635fe3c89985e97d3756ae08
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2020-07-21 08:40:52 +0900

    Bump version: 1.0.4 → 1.1.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hotoku/jmanager',
-    version='1.0.4',
+    version='1.1.0',
     zip_safe=False,
 )
